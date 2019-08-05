c0b31f686934a7a592ec598ff01f338c3591b737
Armand Giraud <armand.giraud.ag@gmail.com>
2019-08-05 13:56:32 +0200

    change max words

--- packages/code-du-travail-nlp/api/suggest.py
+++ packages/code-du-travail-nlp/api/suggest.py
@@ -2,13 +2,12 @@ from flask import request
 from flask import jsonify
 from flask_cors import CORS, cross_origin
 
-
 def add_suggest(app, suggester):
 
   @app.route('/api/suggest', methods=['GET'])
   @cross_origin()
   def suggest():
     input = request.args.get('q')
-    results =  suggester.auto_suggest_fast(input, nb_next_words=12)
+    results =  suggester.auto_suggest_fast(input, nb_next_words=30)
     results = [r[0] for r in results]
     return jsonify(results)
