8c76f6daa3ff8345bd28291188cec12c8df68c92
ManuC <colasante.manuel@gmail.com>
2019-10-25 19:01:20 +0200

    feat(api): add tools endpoint

--- packages/code-du-travail-api/src/server/routes/modeles/searchModele.elastic.js
+++ packages/code-du-travail-api/src/server/routes/modeles/searchModele.elastic.js
@@ -1,13 +1,16 @@
+const { SOURCES } = require("@cdt/sources");
+
 function getModeleBody() {
   return {
     size: 1000,
     _source: ["title", "slug", "description", "editor", "filename"],
+    sort: [{ position: { order: "asc" } }],
     query: {
       bool: {
         filter: [
           {
             term: {
-              source: "modeles_de_courriers"
+              source: SOURCES.LETTERS
             }
           }
         ]
---
+++ packages/code-du-travail-api/src/server/routes/tools/search.elastic.js
@@ -0,0 +1,49 @@
+const { SOURCES } = require("@cdt/sources");
+
+const _source = ["title", "slug", "description"];
+
+function getRootToolsQuery() {
+  return {
+    _source,
+    sort: [{ position: { order: "asc" } }],
+    query: {
+      bool: {
+        filter: [
+          {
+            term: {
+              source: SOURCES.TOOLS
+            }
+          }
+        ]
+      }
+    }
+  };
+}
+
+function getToolQuery({ slug }) {
+  return {
+    _source,
+    size: 1,
+    query: {
+      bool: {
+        filter: [
+          {
+            term: {
+              source: SOURCES.TOOLS
+            }
+          }
+        ],
+        must: [
+          {
+            match: { slug }
+          }
+        ]
+      }
+    }
+  };
+}
+
+module.exports = {
+  getRootToolsQuery,
+  getToolQuery
+};
